e13aeb6392ba0bf0fb9d516ce363a6f1938b5735
JP <85654561+jp524@users.noreply.github.com>
2023-03-12 20:00:37 -0400

    Add Player factory function and player switching logic

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const GameBoard = (() => {
-  const board = [undefined, 'X', 'O', undefined, 'O', 'X', undefined, 'O', 'O'];
-  // const board = Array(9).fill(undefined);
+  // const board = [undefined, 'X', 'O', undefined, 'O', 'X', undefined, 'O', 'O'];
+  const board = Array(9).fill(undefined);
 
   const displayBoard = () => {
     board.forEach((element, index) => {
@@ -18,16 +18,28 @@ const GameBoard = (() => {
   return { board, displayBoard, placeMarker };
 })();
 
+const Player = (name, marker) => ({ name, marker });
+
 const GameController = (() => {
   const board = GameBoard;
 
+  const playerOne = Player('Player 1', 'X');
+  const playerTwo = Player('Player 2', 'O');
+  const players = [playerOne, playerTwo];
+  let activePlayer = players[0];
+
+  const switchPlayerTurn = () => {
+    activePlayer = activePlayer === players[0] ? players[1] : players[0];
+  };
+
   const onCellClick = (event) => {
     if (event.target.value === 'undefined') {
-      board.placeMarker(event.target.id, 'X');
+      board.placeMarker(event.target.id, activePlayer.marker);
+      switchPlayerTurn();
     }
   };
 
-  const playRound = () => {
+  const startGame = () => {
     const cells = document.querySelectorAll('.cell');
     cells.forEach((cell) => {
       cell.addEventListener('click', (event) => onCellClick(event));
@@ -35,7 +47,7 @@ const GameController = (() => {
   };
 
   board.displayBoard();
-  playRound();
+  startGame();
 })();
 
 const game = GameController;
