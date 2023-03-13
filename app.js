28c4d205f1e954721cc30a698323110f2922d880
JP <85654561+jp524@users.noreply.github.com>
2023-03-12 22:35:15 -0400

    Build end of game logic

--- app.js
+++ app.js
@@ -2,6 +2,16 @@ const GameBoard = (() => {
   // const board = [undefined, 'X', 'O', undefined, 'O', 'X', undefined, 'O', 'O'];
   const board = Array(9).fill(undefined);
 
+  const filterByMarker = (marker) => {
+    const result = [];
+    board.forEach((cell, index) => {
+      if (cell === marker) {
+        result.push(index);
+      }
+    });
+    return result;
+  };
+
   const displayBoard = () => {
     board.forEach((element, index) => {
       const cell = document.getElementById(index);
@@ -15,13 +25,24 @@ const GameBoard = (() => {
     displayBoard();
   };
 
-  return { board, displayBoard, placeMarker };
+  return { filterByMarker, displayBoard, placeMarker };
 })();
 
 const Player = (name, marker) => ({ name, marker });
 
 const GameController = (() => {
   const board = GameBoard;
+  const cells = document.querySelectorAll('.cell');
+  const winningCombos = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [1, 4, 8],
+    [2, 4, 6],
+  ];
 
   const playerOne = Player('Player 1', 'X');
   const playerTwo = Player('Player 2', 'O');
@@ -32,15 +53,37 @@ const GameController = (() => {
     activePlayer = activePlayer === players[0] ? players[1] : players[0];
   };
 
+  const endOfGame = () => {
+    // eslint-disable-next-line no-return-assign, no-param-reassign
+    cells.forEach((cell) => (cell.disabled = true));
+  };
+
+  const displayWMessage = () => {
+    const messages = document.querySelector('.messages');
+    const message = document.createElement('div');
+    message.textContent = `${activePlayer.name} wins!`;
+    messages.appendChild(message);
+  };
+
+  const checkWinner = () => {
+    const playerCells = board.filterByMarker(activePlayer.marker);
+    winningCombos.forEach((combo) => {
+      if (combo.every((cell) => playerCells.includes(cell))) {
+        displayWMessage();
+        endOfGame();
+      }
+    });
+  };
+
   const onCellClick = (event) => {
     if (event.target.value === 'undefined') {
       board.placeMarker(event.target.id, activePlayer.marker);
+      checkWinner();
       switchPlayerTurn();
     }
   };
 
   const startGame = () => {
-    const cells = document.querySelectorAll('.cell');
     cells.forEach((cell) => {
       cell.addEventListener('click', (event) => onCellClick(event));
     });
@@ -50,4 +93,5 @@ const GameController = (() => {
   startGame();
 })();
 
+// eslint-disable-next-line no-unused-vars
 const game = GameController;
